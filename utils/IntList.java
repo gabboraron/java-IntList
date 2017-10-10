68847f8e294704d9ac12bc4ce776a35429343c58
gabboraron <gabboraron@gmail.com>
2017-10-10 12:05:10 +0200

    Add files via upload

--- utils/IntList.java
+++ utils/IntList.java
@@ -1,28 +1,28 @@
 class IntList{
-	public static void main(String[] args){
-		/*if(args.length == 0){
-			int l [] = new int[0];
-			int i = 0;
-		}else{
-			int l [] = new int[args.length];
-			int i = 0;
-			for (String s : args){
-				l[i] = Integer.parseInt(s);
-				++i;
-			}
-		}
-		
-		addInd(l, 3, 4);
-		for(int i : l){
-			System.out.printl(i);
-		}*/
-	}
-	//KONSTANSOK
+	
+	//VÁLTOZÓK
+	int list [] = new int[0];
+	int i = 0;
 	
 	//KONSTRUKTOROK
-	public IntList() {}
-	public IntList(int[] elems) {}
+	public IntList() {
+		i = 0;
+	}
 	
+	public IntList(String[] elems) {
+		list = new int[elems.length];
+		i = 0;
+		for (String j : elems){
+			list[i] = Integer.parseInt(j);
+			++i;
+		}
+	}
+	
+	public static void main(String[] args){
+		IntList(args);
+	}
+	
+	/*
 	public static int[] add(int[] l, int a){
 		l[l.length+1] = a;
 		return l;
@@ -51,5 +51,5 @@ class IntList{
 		
 		return (l);
 	}
-	
+	*/
 }
